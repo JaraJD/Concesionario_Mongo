ce779627a905ae95b809caf78d02779fc4de43a3
JuanJaramilloSalazar <salazarjdj@gmail.com>
2023-03-28 11:30:50 -0500

    Estructura inicial del proyecto

---
+++ Mongo.AppService/Automapper/ConfigurationProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Domain.Entities.Commands;
+using Domain.Entities.Entities;
+using Mongo.DrivenAdapter.EntitiesMongo;
+
+namespace Mongo.AppService.Automapper
+{
+	public class ConfigurationProfile :  Profile
+	{
+		public ConfigurationProfile()
+		{
+			CreateMap<InsertNewMarca, Marca>().ReverseMap();
+			CreateMap<MarcaEntitie, Marca>().ReverseMap();
+			CreateMap<MarcaEntitie, InsertNewMarca>().ReverseMap();
+
+			CreateMap<InsertNewConcesionario, Domain.Entities.Entities.Concesionario>().ReverseMap();
+
+			CreateMap<InsertNewAuto, Auto>().ReverseMap();
+
+		}
+	}
+}
---
+++ Mongo.AppService/Controllers/MarcaController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.RegularExpressions;
+
+namespace Mongo.AppService.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class MarcaController : ControllerBase
+	{
+		private readonly IMarcaUseCase _marcaUseCase;
+		private readonly IMapper _mapper;
+
+		public MarcaController(IMarcaUseCase marcaUseCase, IMapper mapper)
+		{
+			_marcaUseCase = marcaUseCase;
+			_mapper = mapper;
+		}
+
+		[HttpGet]
+		public async Task<List<InsertNewMarca>> Obtener_Listado_Marcas()
+		{
+			return await _marcaUseCase.ObtenerListaMarcas();
+		}
+
+		//[HttpGet("/ObtenerPorId/{id}")]
+		//public async Task<Marca> Obtener_Marca_Id(string id)
+		//{
+		//	return await _marcaUseCase.ObtenerMarcaPorId(id);
+		//}
+
+		[HttpPost]
+		public async Task<InsertNewMarca> Registrar_Marca([FromBody] InsertNewMarca command)
+		{
+			return await _marcaUseCase.AgregarMarca(command);
+		}
+
+		[HttpPut("/ActualizarMarca/{id}")]
+		public async Task<Marca> Actualizar_Marca(string id, [FromBody] InsertNewMarca command)
+		{
+			return await _marcaUseCase.ActualizarMarca(id, _mapper.Map<Marca>(command));
+		}
+
+		[HttpDelete("/BorrarMarca/{id}")]
+		public async Task<Marca> Borrar_Marca(string id)
+		{
+			return await _marcaUseCase.BorrarMarca(id);
+		}
+	}
+}
